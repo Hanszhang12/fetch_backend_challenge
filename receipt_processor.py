656022bd744617dfd69ef377f284509a20a24458
Hanszhang12 <hanszhang2022bear@berkeley.edu>
2024-01-25 15:33:49 -0800

    docker files

--- receipt_processor.py
+++ receipt_processor.py
@@ -112,4 +112,4 @@ def get_receipt_points(id):
     return jsonify({"points": points}), 200 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
